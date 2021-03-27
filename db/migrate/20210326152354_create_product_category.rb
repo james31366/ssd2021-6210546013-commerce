4b31bc0172d23f0b9441776e25770d45b736e447
Vichisorn <james31366@gmail.com>
2021-03-28 00:48:06 +0700

    update migration files error and unfinished registration

--- db/migrate/20210326152354_create_product_category.rb
+++ db/migrate/20210326152354_create_product_category.rb
@@ -1,7 +1,7 @@
 class CreateProductCategory < ActiveRecord::Migration[6.1]
   def change
     create_table :product_categories do |t|
-      t.belongs_to :category
+      t.belongs_to :product
       t.belongs_to :category
       t.timestamps
     end
