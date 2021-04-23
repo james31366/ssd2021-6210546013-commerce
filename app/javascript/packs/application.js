c814ba316be5988a9f2b11c34a09876fa2932845
Vichisorn <james31366@gmail.com>
2021-04-23 23:48:41 +0700

    edit application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,11 +10,6 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
-
-require('login')
-require('signup')
-
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
